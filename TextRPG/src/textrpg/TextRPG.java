8c58328dd1f7d6014f1a4aa021ac705106c066e0
JannTheMan <43139450+JannTheMan@users.noreply.github.com>
2018-10-11 17:28:22 +0200

    Update TextRPG/src/textrpg/TextRPG.java

--- TextRPG/src/textrpg/TextRPG.java
+++ TextRPG/src/textrpg/TextRPG.java
@@ -17,6 +17,7 @@ public class TextRPG {
     public static void main(String[] args) {
         // TODO code application logic here
         //test by mrwhisky
+        //just another test
     }
-    
+
 }
